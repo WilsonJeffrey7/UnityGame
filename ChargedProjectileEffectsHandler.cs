63f84b03335c1e70ab6fc0d54695e682ee9a00f9
WilsonJeffrey7 <90283258+WilsonJeffrey7@users.noreply.github.com>
2023-05-02 17:50:32 -0500

    Update ChargedProjectileEffectsHandler.cs

--- ChargedProjectileEffectsHandler.cs
+++ ChargedProjectileEffectsHandler.cs
@@ -5,13 +5,10 @@ namespace Unity.FPS.Gameplay
 {
     public class ChargedProjectileEffectsHandler : MonoBehaviour
     {
-        [Tooltip("Object that will be affected by charging scale & color changes")]
         public GameObject ChargingObject;
 
-        [Tooltip("Scale of the charged object based on charge")]
         public MinMaxVector3 Scale;
 
-        [Tooltip("Color of the charged object based on charge")]
         public MinMaxColor Color;
 
         MeshRenderer[] m_AffectedRenderers;
